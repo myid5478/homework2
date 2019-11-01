e8bbad0b873adcb224d54fbd3812bcbadae34acc
myid5478 <myid5478@gmail.com>
2019-11-01 10:21:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText h;                //宣告全域變數
+    EditText w;                //宣告全域變數
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        h = (EditText)findViewById(R.id.et1);                           // 取得身高物件
+        w = (EditText)findViewById(R.id.et2);                           // 取得體重物件
+        Button submit = (Button)findViewById(R.id.button);             // 取得按鈕物件 如果ID有誤請回到res->Layout查看預設Button是多少
+
+        // 按下按鈕 觸發事件
+        submit.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View arg0) {
+                //判斷條件 身高 跟 體重 都有輸入值才執行
+                if ( !("".equals(h.getText().toString())
+                        || "".equals(w.getText().toString())) )
+                {
+                    float fh = Float.parseFloat(h.getEditableText().toString());      // 取得 身高輸入值
+                    float fw = Float.parseFloat(w.getEditableText().toString());     // 取得 體重輸入值
+                    float fresult;                                     // BMI值 計算結果
+                    TextView result = (TextView)findViewById(R.id.et3);// 取得 顯示結果 物件
+                    fh = fh/100; // 計算BMI
+                    fh = fh*fh;  // 計算BMI
+
+                    NumberFormat nf = NumberFormat.getInstance();   // 數字格式
+                    nf.setMaximumFractionDigits(2);                 // 限制小數第二位
+                    fresult = fw/fh;                                // 計算BMI
+                    result.setText(nf.format(fw/fh) +"");           // 顯示BMI計算結果
+
+                }
+            }
+        });
+    }
+}
